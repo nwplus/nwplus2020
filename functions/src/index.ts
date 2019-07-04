8e9348e5e6ced107c75a06d3cc1d290ce2b2a82b
Giulio Rossi <ciuffi9@mac.com>
2019-07-04 16:57:10 -0700

    Mailchimp subscribe function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
 import * as functions from 'firebase-functions';
-
+const Mailchimp = require('mailchimp-api-v3');
+const API_KEY: string = functions.config().mailchimp.key
+const mailchimp = new Mailchimp(API_KEY)
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const SubscribeToMailingList = functions.https.onRequest(async (request, response) => {
+    if (request.body.email_address == undefined ||
+        request.body.first_name == undefined ||
+        request.body.last_name == undefined){
+            response.sendStatus(500)
+            return
+        }
+    try {
+        await mailchimp.post('lists/01b28e7322/', {
+            members: [
+                {
+                    "email_address": request.body.email_address,
+                    "status": 'subscribed',
+                    'merge_fields': {
+                        'First Name': request.body.first_name,
+                        'Last Name': request.body.last_name
+                    }
+                },
+            ]})
+    }catch (e){
+        console.log('error!!!!')
+        console.log(JSON.stringify(e.errors, null, 4))
+    }
+    response.sendStatus(200)
+});
