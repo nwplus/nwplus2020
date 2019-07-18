c8a1ea155ffedc55e218cdfb30b6a89b68518a3b
Ciuffi <ciuffi9@mac.com>
2019-07-18 12:04:32 -0700

    add new models

---
+++ models/Events.ts
@@ -0,0 +1,11 @@
+interface Events {
+    //Link to the image to show in this event
+    imageLink: string,
+    //Link that the learn more button will link to
+    learnMoreLink: string,
+    //Description of this event
+    text: string,
+    //Title of the event
+    title: string
+
+}
