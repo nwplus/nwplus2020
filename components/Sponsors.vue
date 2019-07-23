fc12263aa6e5a9a836a20d7b0e6282c6003609f1
Luce <lucy_liu@live.ca>
2019-07-22 23:00:24 -0400

    Center Sponsors h2

--- components/Sponsors.vue
+++ components/Sponsors.vue
@@ -2,18 +2,9 @@
   <div id="sponsors">
     <h2>Sponsors</h2>
     <div id="sponsors-list" :class="'is-flex'">
-      <div
-        v-for="item in items"
-        :key="item.name"
-      >
-        <a
-          :href="item.url"
-          target="_blank"
-        >
-          <img
-            :src="item.imageURL"
-            :alt="item.name"
-          >
+      <div v-for="item in items" :key="item.name">
+        <a :href="item.url" target="_blank">
+          <img :src="item.imageURL" :alt="item.name">
         </a>
       </div>
     </div>
@@ -22,38 +13,39 @@
 
 <script>
 export default {
-  props: { items: {
-    type: Array,
-    required: true
-  }
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
   }
 }
 </script>
 
 <style>
-  .sponsors {
-    display: flex;
-    justify-content: space-around;
-  }
-  #sponsors {
-    text-align: center;
-  }
-  #sponsors h2 {
-  width: 30%;
+.sponsors {
+  display: flex;
+  justify-content: space-around;
+}
+#sponsors {
+  text-align: center;
+}
+#sponsors h2 {
+  width: 100%;
   left: 10%;
   height: 45px;
   font-size: 32px;
   line-height: 40px;
   font-weight: 630;
   margin-bottom: 3%;
-  color: #21258A;
-  }
-  #sponsors-list {
-    justify-content: space-evenly;
-  }
-  #sponsors img {
-    height: auto;
-    max-width: 200px;
-    padding: 5px;
-  }
+  color: #21258a;
+}
+#sponsors-list {
+  justify-content: space-evenly;
+}
+#sponsors img {
+  height: auto;
+  max-width: 100px;
+  padding: 5px;
+}
 </style>
