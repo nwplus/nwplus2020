f1a56dba72fc0bff20d45a805b266e4b82ebbf4f
Luce <lucy_liu@live.ca>
2019-08-07 22:06:24 -0400

    Remove docs/ from .gitignore

--- docs/buefy.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import Buefy from 'buefy'
-
-Vue.use(Buefy, {
-  'css': true,
-  'materialDesignIcons': true,
-  'materialDesignIconsHRef': '//cdn.materialdesignicons.com/2.4.85/css/materialdesignicons.min.css'
-})
